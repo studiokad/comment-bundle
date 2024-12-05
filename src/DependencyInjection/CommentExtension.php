a7eec3bc6cc9c5a07f44e0f8c80afb762e081f78
StudioKad <studiokad75@gmail.com>
2024-12-05 03:04:43 +0100

    Initial commit

---
+++ src/DependencyInjection/CommentExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kader\CommentBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class CommentExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        // Chargez les services configurés pour le bundle
+        $loader = new \Symfony\Component\DependencyInjection\Loader\YamlFileLoader(
+            $container,
+            new \Symfony\Component\Config\FileLocator(__DIR__ . '/../Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
